4c62ec7f0685584153de5c28ae06b59d1d31812a
Krithik Rao <krit2000@live.com>
2017-02-04 16:17:36 -0600

    Added brake

--- src/main/java/org/usfirst/frc/team4213/rawsystems/Drivetrain.java
+++ src/main/java/org/usfirst/frc/team4213/rawsystems/Drivetrain.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team4213.rawsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 
@@ -9,10 +11,14 @@ public class Drivetrain {
     private final SpeedController leftMotor;
 
     private final SpeedController rightMotor;
+    
+    private final DoubleSolenoid brake; 
 
     private Drivetrain () {
         leftMotor = new Talon(Integer.parseInt(System.getProperty("left.motor.channel")));
         rightMotor = new Talon(Integer.parseInt(System.getProperty("right.motor.channel")));
+        brake = new DoubleSolenoid(Integer.parseInt(System.getProperty("brake.solenoid.forward.channel")),Integer.parseInt(System.getProperty("brake.solenoid.reverse.channel")));
+       
     }
 
     public void setLeftSpeed(double speed){
@@ -22,4 +28,13 @@ public class Drivetrain {
     public void setRightSpeed(double speed){
     	rightMotor.set(speed);
     }
+    
+    public void setBrakeOpen(boolean open){
+    	if(open) {
+    		brake.set(Value.kReverse); 
+    	} else {
+    		brake.set(Value.kForward);
+    	}
+    }
+  
 }
